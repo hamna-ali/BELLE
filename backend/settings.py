d750d666e7fcea04f610f6698f95278fd810998d
hamna-ali <hamnaali435@gmail.com>
2025-09-24 22:51:33 +0500

    final before deployment

--- backend/settings.py
+++ backend/settings.py
@@ -87,16 +87,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.SessionAuthentication',   # <-- allow admin session login
-#         'rest_framework.authentication.BasicAuthentication',     # <-- optional (for browsable API)
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',  # <-- for your React app
-#     )
-# }
+
 REST_FRAMEWORK = {
     # ✅ Authentication (keep your existing ones so nothing breaks)
     "DEFAULT_AUTHENTICATION_CLASSES": (
